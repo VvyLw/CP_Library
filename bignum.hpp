38f86548d5f3327d44d1474a0ea9d5514676509e
VvyLw <121868689+VvyLw@users.noreply.github.com>
2023-07-09 07:53:27 +0900

    Add files via upload

---
+++ bignum.hpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+typedef __int128_t i128;
+typedef __uint128_t u128;
+
+ostream &operator<<(ostream &dest, i128 value) {
+    ostream::sentry s(dest);
+    if (s) {
+        u128 tmp = value < 0 ? -value : value;
+        char buffer[128];
+        char *d = end(buffer);
+        do {
+            --d;
+            *d = "0123456789"[tmp % 10];
+            tmp /= 10;
+        } while (tmp != 0);
+        if (value < 0) {
+            --d;
+            *d = '-';
+        }
+        int len = end(buffer) - d;
+        if (dest.rdbuf()->sputn(d, len) != len) {
+            dest.setstate(ios_base::badbit);
+        }
+    }
+    return dest;
+}
+inline i128 stoL(string &s) {
+    i128 ret = 0;
+    for(auto el: s) {
+        if(isdigit(el)) ret = 10 * ret + el - '0';
+    }
+    return ret;
+}
