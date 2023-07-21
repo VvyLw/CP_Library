564c4f7072f0e91023acc46c9819556dd03a1a14
VvyLw <121868689+VvyLw@users.noreply.github.com>
2023-07-22 03:47:57 +0900

    Update bignum.hpp

--- C++/bignum.hpp
+++ C++/bignum.hpp
@@ -5,7 +5,7 @@ typedef __int128_t i128;
 typedef __uint128_t u128;//*/
 ostream &operator<<(ostream &dest, i128 value) {
     ostream::sentry s(dest);
-    if (s) {
+    if(s) {
         u128 tmp = value < 0 ? -value : value;
         char buffer[128];
         char *d = end(buffer);
@@ -13,13 +13,13 @@ ostream &operator<<(ostream &dest, i128 value) {
             --d;
             *d = "0123456789"[tmp % 10];
             tmp /= 10;
-        } while (tmp != 0);
-        if (value < 0) {
+        } while(tmp);
+        if(value < 0) {
             --d;
             *d = '-';
         }
         int len = end(buffer) - d;
-        if (dest.rdbuf()->sputn(d, len) != len) {
+        if(dest.rdbuf()->sputn(d, len) != len) {
             dest.setstate(ios_base::badbit);
         }
     }
