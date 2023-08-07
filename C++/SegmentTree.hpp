e505ca7df91169ea3e9170fcecd9aaa09183c873
VvyLw <121868689+VvyLw@users.noreply.github.com>
2023-08-08 00:53:18 +0900

    Update SegmentTree.hpp

--- C++/SegmentTree.hpp
+++ C++/SegmentTree.hpp
@@ -1,38 +1,90 @@
+// inspired by tatyam(https://github.com/tatyam-prime/kyopro_library/blob/master/SegmentTree.cpp)
 #pragma once
 
 #include <vector>
 #include <functional>
 using namespace std;
 typedef long long ll;
-template <class X> struct SegTree {
-    using FX = function<X(X, X)>;
-    ll n;
-    FX fx;
-    const X ex;
-    vector<X> dat;
-    SegTree(ll n_, FX fx_, X ex_) : n(), fx(fx_), ex(ex_), dat(n_ * 4, ex_) {
-        ll x = 1;
-        while (n_ > x) x *= 2;
-        n = x;
+template <class T> struct SegTree {
+    using F = function<T(T, T)>;
+    ll n, rank, fine;
+    const F f;
+    const T e;
+    vector<T> dat;
+    SegTree(const ll n_, const F f_, const T& e_): f(f_), e(e_), fine(n_) {
+        n=1,rank=0;
+        while(fine>n) n<<=1LL,rank++;
+        dat.assign(2*n,e_);
     }
-    void set(ll i, X x) { dat[i+n-1] = x; }
-    void build() { for(int i = n - 2; i >= 0; i--) dat[i] = fx(dat[2*i+1], dat[2*i+2]); }
-    void update(ll i, X x) {
-        i+=n-1;
+    SegTree(const vector<T> &v, const F f_, const T& e_): f(f_), e(e_), fine(v.size()) {
+        n=1,rank=0;
+        while(fine>n) n<<=1LL, rank++;
+        dat.assign(2*n,e_);
+        for(size_t i = 0; i < v.size(); ++i) dat[i+n]=v[i];
+    }
+    T operator[](ll i) const { return dat[i+n]; }
+    void update(ll i, const T& x) {
+        i+=n;
         dat[i]=x;
-        while(i > 0) {
-            i=(i-1)/2;  // parent
-            dat[i]=fx(dat[i * 2 + 1], dat[i * 2 + 2]);
-        }
+        while(i>>=1LL) dat[i]=f(dat[2*i],dat[2*i+1]);
     }
-    X query(ll a, ll b) { return query_sub(a, b, 0, 0, n); }
-    X query_sub(ll a, ll b, ll k, ll l, ll r) {
-        if(r<=a || b<=l) return ex;
-        else if(a<=l && r<=b) return dat[k];
-        else {
-            X vl = query_sub(a,b,k*2+1,l,(l+r)/2);
-            X vr = query_sub(a,b,k*2+2,(l+r)/2,r);
-            return fx(vl, vr);
+    void add(ll i, const T& x) {
+        i+=n;
+        dat[i]+=x;
+        while(i>>=1LL) dat[i]=f(dat[2*i],dat[2*i+1]);
+    }
+    T query(ll a, ll b) const {
+        T l=e,r=e;
+        for(a+=n, b+=n; a<b; a>>=1LL,b>>=1LL) {
+            if(a&1) l=f(l,dat[a++]);
+            if(b&1) r=f(dat[--b],r);
         }
+        return f(l,r);
+    }
+    template <class Boolean=bool> inline int find_left(ll r, const Boolean &fn) {
+        if(!r) return 0;
+        ll h=0,i=r+n;
+        T val=e;
+        for(; h <= rank; h++) if(i>>h&1){
+            T val2=f(val,dat[i>>h^1]);
+            if(fn(val2)){
+                i -= 1<<h;
+                if(i==n) return 0;
+                val=val2;
+            }
+            else break;
+        }
+        for(; h--;){
+            T val2 = f(val,dat[(i>>h)-1]);
+            if(fn(val2)){
+                i-=1<<h;
+                if(i==n) return 0;
+                val=val2;
+            }
+        }
+        return i-n;
+    }
+    template <class Boolean=bool> inline int find_right(ll l, const Boolean &fn) {
+        if(l==fine) return fine;
+        ll h=0,i=l+n;
+        T val=e;
+        for(; h<=rank; h++) if(i>>h&1){
+            T val2=f(val,dat[i>>h]);
+            if(fn(val2)){
+                i+=1LL<<h;
+                if(i==n*2) return fine;
+                val=val2;
+            }
+            else break;
+        }
+        for(; h--;){
+            T val2=f(val, dat[i>>h]);
+            if(fn(val2)){
+                i+=1LL<<h;
+                if(i==n*2) return fine;
+                val=val2;
+            }
+        }
+        return min(i-n,fine);
     }
 };
