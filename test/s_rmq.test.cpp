cf7040a51a8df622650a0f944fc45a18dce44545
VvyLw <owl1465fu9@gmail.com>
2023-08-28 22:46:56 +0900

    ok

---
+++ test/s_rmq.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
+/*#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")//*/
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <C++/template.hpp>
+#include <C++/SparseTable.hpp>
+using namespace zia_qu;
+using namespace Lady_sANDy;
+using namespace Heileden;
+int main() {
+  	VvyLw::wa_haya_exe();
+  	now(start);
+  	//INT(t); while(t--)
+  	VvyLw::solve();
+  	now(stop);
+  	time(start, stop);
+}
+
+// --------------------------------------------------------------------------------------------------------------
+
+void VvyLw::solve() {
+    INT(n,q);
+    VEC(int,a,n);
+    SparseTable<int> st(a,[](int a, int b){ return min(a, b); });
+    while(q--) {
+        INT(l,r);
+        out(st.query(l, r));
+    }
+}
