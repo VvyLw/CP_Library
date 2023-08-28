36700e921eee864f46f151f350afea55723c115f
VvyLw <owl1465fu9@gmail.com>
2023-08-28 16:11:28 +0900

    added libtest

---
+++ libtest/add.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+/*#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")//*/
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <template.hpp>
+using namespace zia_qu;
+using namespace Lady_sANDy;
+using namespace Heileden;
+int main() {
+  VvyLw::wa_haya_exe();
+  now(start);
+  /*INT(t); while(t--)//*/
+  VvyLw::solve();
+  now(stop);
+  time(start, stop);
+}
+
+// --------------------------------------------------------------------------------------------------------------
+
+void VvyLw::solve() {
+    INT(a+b);
+    out(a+b);
+}
