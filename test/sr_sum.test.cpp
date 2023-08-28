c59f0b4ba6f7f689576e372d8aebe4383957976d
VvyLw <owl1465fu9@gmail.com>
2023-08-28 22:40:04 +0900

    ok

---
+++ test/sr_sum.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_sum"
+/*#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")//*/
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <C++/template.hpp>
+using namespace zia_qu;
+using namespace Lady_sANDy;
+using namespace Heileden;
+int main() {
+  	VvyLw::wa_haya_exe();
+  	now(start);
+  	//INT(t); while(t--)
+  	VvyLw::solve();
+  	now(stop);
+  	time(start, stop);
+}
+
+// --------------------------------------------------------------------------------------------------------------
+
+void VvyLw::solve() {
+    INT(n,q);
+    VEC(ll,a,n);
+    auto s=psum(a);
+    while(q--) {
+        INT(l,r);
+        out(s[r]-s[l]);
+    }
+}
