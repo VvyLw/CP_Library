da21b4e0d0ef86953935789c6e4a0fc82c5b1853
VvyLw <owl1465fu9@gmail.com>
2023-08-28 22:28:25 +0900

    ok

---
+++ test/uf.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+/*#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")//*/
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <C++/template.hpp>
+#include <C++/UnionFind.hpp>
+using namespace zia_qu;
+using namespace Lady_sANDy;
+using namespace Heileden;
+int main() {
+  	VvyLw::wa_haya_exe();
+  	now(start);
+  	//INT(t); while(t--)
+  	VvyLw::solve();
+  	now(stop);
+  	time(start, stop);
+}
+
+// --------------------------------------------------------------------------------------------------------------
+
+void VvyLw::solve() {
+    INT(n,q);
+    UnionFind uf(n);
+    while(q--) {
+        INT(t,a,b);
+        if(t) out(uf[a]==uf[b]);
+        else uf.unite(a,b);
+    }
+}
