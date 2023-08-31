0e29edc26bb5478c794da00808ea6b2cd311b810
VvyLw <owl1465fu9@gmail.com>
2023-08-31 19:53:12 +0900

    add some function and module

--- V/template.v
+++ V/template.v
@@ -1,12 +1,36 @@
-import os
-fn si() string { return os.input('') }
-fn ni() i64 { return si().i64() }
+import os { input }
+//import math as ma
+import arrays as ar
+//import datatypes as dt
+import regex as re
+const (
+	mod998=998244353
+	mod107=1000000007
+	inf=1<<30
+	linf=1<<61
+	dx=[0,0,0,-1,1,-1,-1,1,1]
+	dy=[0,-1,1,0,0,-1,1,-1,1]
+)
+fn si() string { return input('') }
+fn ni() int { return si().int() }
 fn di() f64 { return si().f64() }
-fn lsi() []string { return os.get_line().split(' ') }
-fn lni() []i64 { return os.get_line().split(' ').map(it.i64()) }
-fn ldi() []f64 { return os.get_line().split(' ').map(it.f64()) }
+fn bi() i64 { return si().i64() }
+fn lsi() []string { return si().split(' ') }
+fn lni() []int { return si().split(' ').map(it.int()) }
+fn ldi() []f64 { return si().split(' ').map(it.f64()) }
+fn lbi() []i64 { return si().split(' ').map(it.i64()) }
+fn dbg[T](x T) { eprintln(x) }
 fn toascii(n i64) string { return u8(n).ascii_str() }
 fn iota(n i64, dlt i64) []i64 { return []i64{len: int(n), init: index+dlt} }
-pub fn main() void {
+fn sorted[T](a []T) []T { mut b:=a.clone() b.sort() return b }
+fn resorted[T](a []T) []T { mut b:=a.clone() b.sort(a>b) return b }
+fn mod(n int, m int) int { return (n+m)%m }
+fn bs[T](a []T, x T) !int { return ar.binary_search(a,x)! }
+fn rgxmt(s string, pat string) !bool { r:=re.regex_opt(pat)! return r.matches_string(s) }
+struct Pair[T,U] {
+	first T
+	second U
+}
+pub fn main() {
 	
 }
