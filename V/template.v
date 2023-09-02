c0070870eedf3903ed450360fec8473a34330446
VvyLw <owl1465fu9@gmail.com>
2023-09-03 08:07:44 +0900

    add some functions

--- V/template.v
+++ V/template.v
@@ -32,6 +32,10 @@ fn no(ok bool) { yes(!ok) }
 fn chmin[T, U](mut a T, b U) bool { jdg:=a>b a=ma.min(a,b) return jdg }
 fn chmax[T, U](mut a T, b U) bool { jdg:=a<b a=ma.max(a,b) return jdg }
 fn toc(n i64) string { return u8(n).ascii_str() }
+fn sygcd(a ...i64) i64 { mut g:=i64(0) for el in a { g=ma.gcd(g,el) } return g }
+fn sylcm(a ...i64) i64 { mut l:=i64(1) for el in a { l=ma.lcm(l,el) } return l }
+fn symin(a ...i64) i64 { mut res:=ma.maxof[i64]() for el in a { res=ma.min(res,el) } return res }
+fn symax(a ...i64) i64 { mut res:=ma.minof[i64]() for el in a { res=ma.max(res,el) } return res }
 fn iota(n int, dlt i64) []i64 { return []i64{len: n, init: index+dlt} }
 fn mod(n int, m int) int { return (n+m)%m }
 fn bs[T](a []T, x T) !int { return ar.binary_search(a,x)! }
