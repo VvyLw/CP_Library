2ca36dff7b60816960299bc5450fcd6ea5eb4846
VvyLw <owl1465fu9@gmail.com>
2023-09-03 02:03:46 +0900

    add and modified some functions

--- V/template.v
+++ V/template.v
@@ -1,8 +1,8 @@
 import os { input }
-//import math as ma
+import math as ma
 import arrays as ar
-//import datatypes as dt
 import regex as re
+//import datatypes as dt
 const (
 	mod998=998244353
 	mod107=1000000007
@@ -11,6 +11,8 @@ const (
 	dx=[0,0,0,-1,1,-1,-1,1,1]
 	dy=[0,-1,1,0,0,-1,1,-1,1]
 )
+type F1[T]=fn(T)bool
+type F2[T]=fn(T,T)bool
 fn si() string { return input('') }
 fn ni() int { return si().int() }
 fn di() f64 { return si().f64() }
@@ -19,13 +21,22 @@ fn lsi() []string { return si().split(' ') }
 fn lni() []int { return si().split(' ').map(it.int()) }
 fn ldi() []f64 { return si().split(' ').map(it.f64()) }
 fn lbi() []i64 { return si().split(' ').map(it.i64()) }
-fn dbg[T](x T) { eprintln(x) }
-fn toascii(n i64) string { return u8(n).ascii_str() }
-fn iota(n i64, dlt i64) []i64 { return []i64{len: int(n), init: index+dlt} }
-fn sorted[T](a []T) []T { mut b:=a.clone() b.sort() return b }
-fn resorted[T](a []T) []T { mut b:=a.clone() b.sort(a>b) return b }
+fn aro[T](a []T) string { return a.map(it.str()).join(' ') }
+fn dbg[T](args ...T) {
+	eprintln('---------------------')
+	for id, el in args { eprintln('arg${id}: ${el}') }
+	eprintln('---------------------')
+}
+fn yes(ok bool) { println(if ok {'Yes'} else {'No'}) }
+fn no(ok bool) { yes(!ok) }
+fn chmin[T, U](mut a T, b U) bool { jdg:=a>b a=ma.min(a,b) return jdg }
+fn chmax[T, U](mut a T, b U) bool { jdg:=a<b a=ma.max(a,b) return jdg }
+fn toc(n i64) string { return u8(n).ascii_str() }
+fn iota(n int, dlt i64) []i64 { return []i64{len: n, init: index+dlt} }
 fn mod(n int, m int) int { return (n+m)%m }
 fn bs[T](a []T, x T) !int { return ar.binary_search(a,x)! }
+fn a_find[T](a []T, filt F1[T]) ?T { return ar.find_first(a,filt) }
+fn a_filter[T](a []T, filt F2[T]) []T { return ar.filter_indexed(a,filt) }
 fn rgxmt(s string, pat string) !bool { r:=re.regex_opt(pat)! return r.matches_string(s) }
 struct Pair[T,U] {
 	first T
