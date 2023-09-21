8d90aeb1c88d8525702cdf67f4bfddcd722b8b57
VvyLw <owl1465fu9@gmail.com>
2023-09-21 15:34:40 +0900

    added large and stol functions

--- V/template.v
+++ V/template.v
@@ -1,5 +1,6 @@
 import os { input }
 import math as ma
+import math.big
 import arrays as ar
 import strings as str
 import regex as re
@@ -44,12 +45,31 @@ fn no(ok bool) { yes(!ok) }
 fn chmin[T, U](mut a T, b U) bool { jdg:=a>b a=ma.min(a,b) return jdg }
 fn chmax[T, U](mut a T, b U) bool { jdg:=a<b a=ma.max(a,b) return jdg }
 fn toc(n i64) string { return u8(n).ascii_str() }
+fn cas(s string) int { return s[0] }
 fn sygcd(a ...i64) i64 { mut g:=i64(0) for el in a { g=ma.gcd(g,el) } return g }
 fn sylcm(a ...i64) i64 { mut l:=i64(1) for el in a { l=ma.lcm(l,el) } return l }
 fn symin(a ...i64) i64 { mut res:=ma.maxof[i64]() for el in a { res=ma.min(res,el) } return res }
 fn symax(a ...i64) i64 { mut res:=ma.minof[i64]() for el in a { res=ma.max(res,el) } return res }
-fn iota(n int, dlt i64) []i64 { return []i64{len: n, init: index+dlt} }
 fn mod(n i64, m int) i64 { return (n+m)%m }
+fn large(n i64) big.Integer { return big.integer_from_i64(n) }
+fn stol(s string) !big.Integer { return big.integer_from_string(s)! }
+fn iota(n int, dlt i64) []i64 { return []i64{len: n, init: index+dlt} }
+fn unq[T](a []T) []T {
+	mut res := []T{cap: a.len / 10}
+	mut j := -1
+	if a.len > 0 {
+		j = 0
+		res << a[0]
+	}
+	for idx, e in a {
+		if a[j] == e {
+			continue
+		}
+		j = idx
+		res << e
+	}
+	return res
+}
 fn bs[T](a []T, x T) !int { return ar.binary_search(a,x)! }
 fn a_find[T](a []T, filt F1[T]) ?T { return ar.find_first(a,filt) }
 fn a_filter[T](a []T, filt F2[T]) []T { return ar.filter_indexed(a,filt) }
