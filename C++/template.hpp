ac70b9f616636430e1a8c171cfa54db9a6c25660
VvyLw <owl1465fu9@gmail.com>
2023-11-09 16:48:01 +0900

    map function

--- C++/template.hpp
+++ C++/template.hpp
@@ -391,19 +391,19 @@ template <class K, class V> inline K kymax(const map<K,V> &m){ return m.rbegin()
 template <class K, class V> inline V kymin_v(const map<K,V> &m){ return m.begin()->second; }
 template <class K, class V> inline V kymax_v(const map<K,V> &m){ return m.rbegin()->second; }
 template <class K, class V> inline V vlmin(const map<K,V> &m){
-  auto pr = *min_element(all(m),[](P<K,V> const &x, P<K,V> const &y){ return x.second > y.second; });
+  const auto pr = *min_element(all(m),[](P<K,V> const &x, P<K,V> const &y){ return x.second < y.second; });
   return pr.second;
 }
 template <class K, class V> inline V vlmax(const map<K,V> &m){
-  auto pr = *max_element(all(m),[](P<K,V> const &x, P<K,V> const &y){ return x.second < y.second; });
+  const auto pr = *max_element(all(m),[](P<K,V> const &x, P<K,V> const &y){ return x.second < y.second; });
   return pr.second;
 }
 template <class K, class V> inline K vlmin_k(const map<K,V> &m){
-  auto pr = *min_element(all(m),[](P<K,V> const &x, P<K,V> const &y){ return x.second > y.second; });
+  const auto pr = *min_element(all(m),[](P<K,V> const &x, P<K,V> const &y){ return x.second < y.second; });
   return pr.first;
 }
 template <class K, class V> inline K vlmax_k(const map<K,V> &m){
-  auto pr = *max_element(all(m),[](P<K,V> const &x, P<K,V> const &y){ return x.second < y.second; });
+  const auto pr = *max_element(all(m),[](P<K,V> const &x, P<K,V> const &y){ return x.second < y.second; });
   return pr.first;
 }
 } // zia qu
