ffc44f9e3f62a4c15baa023de20f944634e1fdd0
VvyLw <owl1465fu9@gmail.com>
2023-11-12 00:36:03 +0900

    new Edge

--- C++/graph.hpp
+++ C++/graph.hpp
@@ -132,3 +132,32 @@ template <bool undirected=1> struct graph {
         return d[v];
     }
 };
+// #include <UnionFind.hpp>
+struct Edge {
+	ll a, b, cost;
+	bool operator<(const Edge& e) const {
+		return cost < e.cost;
+	}
+};
+struct Graph {
+	int n;
+	V<Edge> edges;
+	Graph(const int n_): n(n_){}
+	void input(ll m, uint indexed=1) {
+		rep(m) {
+			Edge e;
+			in(e.a,e.b,e.cost);
+			e.a-=indexed, e.b-=indexed;
+			edges.emplace_back(e);
+		}
+	}
+	ll kruskal() {
+		Lady_sANDy::Sort(edges);
+		UnionFind uf(n);
+		ll res=0;
+		each(ed,edges) {
+			if(uf.unite(ed.a,ed.b)) res+=ed.cost;
+		}
+		return res;
+	}
+};
