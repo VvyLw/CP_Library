3127d01fe29e2e5e0d2afb2a113330b5356d4822
VvyLw <owl1465fu9@gmail.com>
2023-11-28 10:57:12 +0900

    C++20/17

--- C++/UnionFind.hpp
+++ C++/UnionFind.hpp
@@ -1,39 +1,49 @@
 // inspired by maspy(https://github.com/maspypy/library/blob/main/ds/unionfind/unionfind.hpp)
 #pragma once
-
+#include <cassert>
 #include <vector>
 #include <algorithm>
-using namespace std;
-typedef long long ll;
 struct UnionFind {
 private:
-    vector<ll> par;
+    std::vector<int> par;
 public:
-    UnionFind(ll n): par(n, -1){}
-    ll operator[](ll a) {
-        while(par[a] >= 0) {
-            ll p = par[par[a]];
-            if(p < 0) return par[a];
-            a = par[a] = p;
+    UnionFind(const int n): par(n, -1){}
+    int operator[](int i) {
+        while(par[i] >= 0) {
+            const int p = par[par[i]];
+            if(p < 0) return par[];
+            i = par[a] = p;
         }
-        return a;
+        return i;
     }
-    bool unite(ll x, ll y) {
+    bool unite(int x, int y) {
         x = (*this)[x], y = (*this)[y];
-        if(x == y) return 0;
-        if(-par[x] < -par[y]) swap(x, y);
+        if(x == y) return false;
+        if(-par[x] < -par[y]) std::swap(x, y);
         par[x] += par[y], par[y] = x;
-        return 1;
+        return true;
     }
-    ll size(ll x) {
-        x = (*this)[x];
-        return -par[x];
-    }
-    vector<vector<ll>> groups() {
-        const int n = par.size();
-        vector<vector<ll>> res(n);
-        for(int i = 0; i < n; ++i) res[(*this)[i]].emplace_back(i);
-        res.erase(remove_if(res.begin(), res.end(), [&](const vector<ll> &v){ return v.empty(); }), res.end());
+    int size(const int x){ return -par[(*this)[x]]; }
+#if __cplusplus >= 202101L
+    std::vector<std::vector<int>> groups() {
+        const int n = std::ssize(par);
+        std::vector<std::vector<int>> res(n);
+        for(int i = 0; i < n; ++i) {
+            res[(*this)[i]].emplace_back(i);
+        }
+        const auto it = std::ranges::remove_if(res, [&](const std::vector<int> &v){ return v.empty(); });
+        res.erase(it.begin(), it.end());
         return res;
     }
+#else
+    std::vector<std::vector<int>> groups() {
+        const int n = par.size();
+        std::vector<std::vector<int>> res(n);
+        for(int i = 0; i < n; ++i) {
+            res[(*this)[i]].emplace_back(i);
+        }
+        res.erase(std::remove_if(res.begin(), res.end(), [&](const std::vector<int> &v){ return v.empty(); }), res.end());
+        return res;
+    }
+#endif
 };
