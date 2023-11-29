bafb6f62203e9b0cb6dd763f516a99104a6012c1
VvyLw <owl1465fu9@gmail.com>
2023-11-29 21:29:14 +0900

    pf verify

---
+++ test/pf.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/factorize"
+#include <iostream>
+#include <map>
+#include <C++/Pollards_rho.hpp>
+int main() {
+    std::cin.tie(nullptr) -> sync_with_stdio(false);
+    int q;
+    std::cin >> q;
+    std::map<ll, std::vector<ll>> m;
+    const auto solve = [&]{
+        ll n;
+        std::cin >> n;
+        if(m.find(n) == m.end()) {
+            auto p = rho(n);
+            std::sort(p.begin(), p.end());
+            m.try_emplace(n, p);
+        }
+        const auto ans = m[n];
+        std::cout << ans.size();
+        if(ans.size()) {
+            std::cout << ' ' << ans.front();
+            for(auto i = ans.begin(); ++i != ans.end();) {
+                std::cout << ' ' << *i;
+            }
+        }
+        std::cout << '\n';
+    };
+    while(q--) {
+        solve();
+    }
+}
