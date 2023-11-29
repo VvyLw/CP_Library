88b377f6fc6dc2932daf294573dd943e0c96f2e3
VvyLw <owl1465fu9@gmail.com>
2023-11-30 04:35:20 +0900

    C++ All

--- Java/template.java
+++ Java/template.java
@@ -1,13 +1,20 @@
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.math.BigInteger;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.TreeMap;
 import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 class VvyLw extends MyFunction {
@@ -144,13 +151,73 @@ class MyFunction {
 	protected static int upperBound(final List<Long> a, final long x) {
 		return ~Collections.binarySearch(a, x, (p, q) -> p.compareTo(q) > 0 ? 1 : -1);
 	}
+	protected static int[] reverse(final int[] a) {
+		final int n = a.length;
+		int[] b = new int[n];
+		for(int i = 0; i <= n / 2; ++i) {
+			b[i] = a[n - 1 - i];
+			b[n - 1 - i] = a[i];
+		}
+		return b;
+	}
+	protected static long[] reverse(final long[] a) {
+		final int n = a.length;
+		long[] b = new long[n];
+		for(int i = 0; i <= n / 2; ++i) {
+			b[i] = a[n - 1 - i];
+			b[n - 1 - i] = a[i];
+		}
+		return b;
+	}
+	protected static double[] reverse(final double[] a) {
+		final int n = a.length;
+		double[] b = new double[n];
+		for(int i = 0; i <= n / 2; ++i) {
+			b[i] = a[n - 1 - i];
+			b[n - 1 - i] = a[i];
+		}
+		return b;
+	}
+	protected static Object[] reverse(final Object[] a) {
+		final int n = a.length;
+		Object[] b = new Object[n];
+		for(int i = 0; i <= n / 2; ++i) {
+			b[i] = a[n - 1 - i];
+			b[n - 1 - i] = a[i];
+		}
+		return b;
+	}
 	protected static int[] rotate(final int[] a, final int id) {
-		ArrayList<Integer> t = new ArrayList<>();
+		ArrayList<Integer> t = new ArrayList<>(a.length);
 		for(final var el: a) {
 			t.add(el);
 		}
 		Collections.rotate(t, id);
 		int[] res = new int[t.size()];
+		for(int i = 0; i < t.size(); ++i) {
+			res[i] = t.get(i);
+		}
+		return res;
+	}
+	protected static long[] rotate(final long[] a, final int id) {
+		ArrayList<Long> t = new ArrayList<>(a.length);
+		for(final var el: a) {
+			t.add(el);
+		}
+		Collections.rotate(t, id);
+		long[] res = new long[t.size()];
+		for(int i = 0; i < t.size(); ++i) {
+			res[i] = t.get(i);
+		}
+		return res;
+	}
+	protected static double[] rotate(final double[] a, final int id) {
+		ArrayList<Double> t = new ArrayList<>(a.length);
+		for(final var el: a) {
+			t.add(el);
+		}
+		Collections.rotate(t, id);
+		double[] res = new double[t.size()];
 		for(int i = 0; i < t.size(); ++i) {
 			res[i] = t.get(i);
 		}
@@ -168,8 +235,50 @@ class MyFunction {
 		}
 		return sb.toString(); 
 	}
+	protected static int[][] rotate(final int[][] a) {
+		final int h = a.length, w = a[0].length;
+		int[][] b = new int[w][h];
+		IntStream.range(0, h).forEach(i -> {
+			IntStream.range(0, w).forEach(j -> b[j][i] = a[i][j]);
+		});
+		IntStream.range(0, w).forEach(i -> b[i] = reverse(b[i]));
+		return b;
+	}
+	protected static long[][] rotate(final long[][] a) {
+		final int h = a.length, w = a[0].length;
+		long[][] b = new long[w][h];
+		IntStream.range(0, h).forEach(i -> {
+			IntStream.range(0, w).forEach(j -> b[j][i] = a[i][j]);
+		});
+		IntStream.range(0, w).forEach(i -> b[i] = reverse(b[i]));
+		return b;
+	}
+	protected static double[][] rotate(final double[][] a) {
+		final int h = a.length, w = a[0].length;
+		double[][] b = new double[w][h];
+		IntStream.range(0, h).forEach(i -> {
+			IntStream.range(0, w).forEach(j -> b[j][i] = a[i][j]);
+		});
+		IntStream.range(0, w).forEach(i -> b[i] = reverse(b[i]));
+		return b;
+	}
+	protected static String[] rotate(final String[] s) {
+		final int h = s.length, w = s[0].length();
+		char[][] t = new char[w][h];
+		IntStream.range(0, h).forEach(i -> {
+			IntStream.range(0, w).forEach(j -> t[j][i] = s[i].charAt(j));
+		});
+		IntStream.range(0, w).forEach(i -> t[i] = new StringBuilder(new String(t[i])).reverse().toString().toCharArray());
+		String[] res = new String[w];
+		IntStream.range(0, w).forEach(i -> res[i] = new String(t[i]));
+		return res;
+	}
 	protected static long lcm(final long a, final long b){ return a * b / gcd(a, b); }
 	protected static long gcd(final long a, final long b){ return b > 0 ? gcd(b, a % b) : a; }
+	protected static long lcm(final long... a){ return Arrays.stream(a).reduce(1, (x, y) -> lcm(x, y)); }
+	protected static long gcd(final long... a){ return Arrays.stream(a).reduce(0, (x, y) -> gcd(x, y)); }
+	protected static long min(final long... a){ return Arrays.stream(a).reduce(Long.MAX_VALUE, (x, y) -> Math.min(x, y)); }
+	protected static long max(final long... a){ return Arrays.stream(a).reduce(Long.MIN_VALUE, (x, y) -> Math.max(x, y)); }
 	protected static <F, S> ArrayList<F> first(final List<Pair<F, S>> p) {
 		ArrayList<F> f = new ArrayList<>();
 		for(final var el: p) {
@@ -183,6 +292,28 @@ class MyFunction {
 			s.add(el.second);
 		}
 		return s;
+	}
+	protected static int[] iota(final int n){ return IntStream.range(0, n).toArray(); }
+	protected static int[] iota(final int n, final int init){ return IntStream.range(0 + init, n + init).toArray(); }
+	protected static long bins(long ok, long ng, final Predicate<Long> fn) {
+		while(Math.abs(ok - ng) > 1) {
+			final long mid = (ok + ng) / 2;
+			if(fn.test(mid)) {
+				ok = mid;
+			}
+			else {
+				ng = mid;
+			}
+		}
+		return ok;
+	}
+	protected static ArrayList<Integer> press(final ArrayList<Long> a) {
+		ArrayList<Integer> res = new ArrayList<>();
+		final var cp = a.stream().sorted().distinct().toList();
+		for(final var el: a) {
+			res.add(lowerBound(cp, el));
+		}
+		return res;
 	}
 }
 
@@ -259,8 +390,11 @@ class MyPrinter {
 		Arrays.stream(tail).forEach(pw::println);
 	}
 	void outl(final int[] args){ Arrays.stream(args).forEach(pw::println); }
+	void outl(final int[][] args){ IntStream.range(0, args.length).forEach(i -> out(args[i])); }
 	void outl(final long[] args){ Arrays.stream(args).forEach(pw::println); }
+	void outl(final long[][] args){ IntStream.range(0, args.length).forEach(i -> out(args[i])); }
 	void outl(final double[] args){ Arrays.stream(args).forEach(pw::println); }
+	void outl(final double[][] args){ IntStream.range(0, args.length).forEach(i -> out(args[i])); }
 	void outl(final char[] args){ IntStream.range(0, args.length).forEach(i -> out(args[i])); }
 	void outl(final Object[] args){ Arrays.stream(args).forEach(pw::println); }
 	<E> void outl(final Collection<E> args){ args.stream().forEach(pw::println); }
@@ -316,7 +450,7 @@ class NumPair extends Pair<Number, Number> {
 	NumPair(final Number first, final Number second){ super(first, second); }
 	NumPair rotate(){ return new NumPair(-second.doubleValue(), first.doubleValue()); } 
 	NumPair rotate(final int ang) {
-		final double rad = Math.PI * MyFunction.mod(ang, 360) / 180;
+		final double rad = Math.toRadians(MyFunction.mod(ang, 360));
 		return new NumPair(first.doubleValue() * Math.cos(rad) - second.doubleValue() * Math.sin(rad),
 							first.doubleValue() * Math.sin(rad) + second.doubleValue() * Math.cos(rad));
 	}
@@ -380,12 +514,8 @@ class UnionFind {
 	ArrayList<ArrayList<Integer>> groups() {
 		final int n = par.length;
 		ArrayList<ArrayList<Integer>> res = new ArrayList<>(n);
-		for(int i = 0; i < n; ++i) {
-			res.add(new ArrayList<>());
-		}
-		for(int i = 0; i < n; ++i) {
-			res.get(root(i)).add(i);
-		}
+		IntStream.range(0, n).forEach(i -> res.add(new ArrayList<>()));
+		IntStream.range(0, n).forEach(i -> res.get(root(i)).add(i));
 		res.removeIf(ArrayList::isEmpty);
 		return res;
 	}
@@ -396,6 +526,191 @@ class UnionFind {
 			ok &= root(i) != root(i + n);
 		}
 		return ok;
+	}
+}
+
+class Edge {
+	public int src;
+	public int to;
+	public long cost;
+	Edge(final int to) {
+		this.to = to;
+	}
+	Edge(final int to, final long cost) {
+		this.to = to;
+		this.cost = cost;
+	}
+	Edge(final int src, final int to, final long cost) {
+		this.src = src;
+		this.to = to;
+		this.cost = cost;
+	}
+}
+class Graph {
+	protected boolean undirected;
+	protected int n, indexed;
+	ArrayList<ArrayList<Edge>> g;
+	Graph(final int n, final int indexed, final boolean undirected) {
+		this.n = n;
+		this.indexed = indexed;
+		this.undirected = undirected;
+		g = new ArrayList<>(n);
+		IntStream.range(0, n).forEach(i -> g.add(new ArrayList<>()));
+	}
+	void add(int a, int b, final long cost) {
+		a -= indexed;
+		b -= indexed;
+		g.get(a).add(new Edge(b));
+		if(undirected) {
+			g.get(b).add(new Edge(a));
+		}
+	}
+	protected int[] allDist(final int v) {
+		int[] d = new int[n];
+		Arrays.fill(d, -1);
+		Queue<Integer> q = new ArrayDeque<>();
+		d[v] = 0;
+		q.add(v);
+		while(!q.isEmpty()) {
+			final int tmp = q.poll();
+			for(final var el: g.get(tmp)) {
+				if(d[el.to] != -1) {
+					continue;
+				}
+				d[el.to]=d[tmp]+1;
+				q.add(el.to);
+			}
+		}
+		return d;
+	}
+	protected int dist(final int u, final int v){ return allDist(u)[v]; }
+}
+class WeightedGraph extends Graph {
+	ArrayList<ArrayList<Edge>> g;
+	WeightedGraph(final int n, final int indexed, final boolean undirected) {
+		super(n, indexed, undirected);
+		g = new ArrayList<>(n);
+		IntStream.range(0, n).forEach(i -> g.add(new ArrayList<>()));
+	}
+	void add(int a, int b, final long cost) {
+		a -= indexed;
+		b -= indexed;
+		g.get(a).add(new Edge(b, cost));
+		if(undirected) {
+			g.get(b).add(new Edge(a, cost));
+		}
+	}
+	long[] dijkstra(final int v) {
+		long[] cost = new long[n];
+		Arrays.fill(cost, Long.MAX_VALUE);
+		Queue<NumPair> dj = new PriorityQueue<>(Collections.reverseOrder());
+		cost[v] = 0;
+		dj.add(new NumPair(cost[v], v));
+		while(!dj.isEmpty()) {
+			final var tmp = dj.poll();
+			if(cost[tmp.second.intValue()] < tmp.first.longValue()) {
+				continue;
+			}
+			for(final var el: g.get(tmp.second.intValue())) {
+				if(cost[el.to] > tmp.first.longValue() + el.cost) {
+					cost[el.to] = tmp.first.longValue() + el.cost;
+					dj.add(new NumPair(cost[el.to], el.to));
+				}
+			}
+		}
+		return cost;
+	}
+	long[][] warshallFloyd() {
+		long[][] cost = new long[n][n];
+		IntStream.range(0, n).forEach(i -> Arrays.fill(cost[i], Long.MAX_VALUE));
+		IntStream.range(0, n).forEach(i -> cost[i][i] = 0);
+		for(int i = 0; i < n; ++i) {
+			for(final var j: g.get(i)) {
+				cost[i][j.to] = j.cost;
+			}
+		}
+		for(int k = 0 ; k < n; ++k) {
+			for(int i = 0; i < n; ++i) {
+				for(int j = 0; j < n; ++j) {
+					if(cost[i][j] > cost[i][k] + cost[k][j]) {
+						cost[i][j] = cost[i][k] + cost[k][j];
+					}
+				}
+			}
+		}
+		return cost;
+	}
+}
+class Tree {
+	public ArrayList<Edge> edge;
+	private int n, indexed;
+	Tree(final int n, final int indexed) {
+		edge = new ArrayList<>(n);
+		this.n = n;
+		this.indexed = indexed;
+	}
+	void add(final Edge e){ edge.add(new Edge(e.src - indexed, e.to - indexed, e.cost)); }
+	long kruskal() {
+		Collections.sort(edge, Comparator.comparing(e -> e.cost));
+		UnionFind uf = new UnionFind(n);
+		long res = 0;
+		for(final var ed: edge) {
+			if(uf.unite(ed.src, ed.to)) {
+				res += ed.cost;
+			}
+		}
+		return res;
+	}
+}
+
+class PrimeTable {
+	private int n;
+	boolean[] sieve;
+	PrimeTable(final int n) {
+		this.n = n;
+		sieve = new boolean[n + 1];
+		Arrays.fill(sieve, true);
+		for(long i = 2; i <= n; ++i) {
+			if(!sieve[(int) i]) {
+				continue;
+			}
+			for(long j = i * i; j <= n; j += i) {
+				sieve[(int) j] = false;
+			}
+		}
+	}
+	ArrayList<Integer> get() {
+		ArrayList<Integer> p = new ArrayList<>();;
+		for(int i = 2; i <= n; ++i) {
+			if(sieve[i]) {
+				p.add(i);
+			}
+		}
+		return p;
+	}
+}
+
+class PrimeFactor {
+	private int[] spf;
+	PrimeFactor(final int n) {
+		spf = IntStream.rangeClosed(0, n).toArray();
+		for(int i = 2; i * i <= n; ++i) {
+			if(spf[i] == i) {
+				for(int j = i * i; j <= n; j += i) {
+					if(spf[j] == j) {
+						spf[j] = i;
+					}
+				}
+			}
+		}
+	}
+	TreeMap<Integer, Integer> get(int n) {
+		TreeMap<Integer, Integer> m = new TreeMap<>();
+		while(n != 1) {
+			m.merge(spf[n], 1, (a, b) -> (a + b));
+			n /= spf[n];
+		}
+		return m;
 	}
 }
 
@@ -487,5 +802,234 @@ class BigPrime {
 		final ArrayList<Long> ri = primeFactor(n / x);
 		le.addAll(ri);
 		return le;
+	}
+}
+
+class FenwickTree {
+	private int n;
+	private long[] data;
+	FenwickTree(final int n) {
+		this.n = n + 2;
+		data = new long[n + 1];
+	}
+	long sum(int k) {
+		if(k < 0) return 0;
+		long ret = 0;
+		for(++k; k > 0; k -= k & -k) {
+			ret += data[k];
+		}
+		return ret;
+	}
+	long sum(final int l, final int r){ return sum(r) - sum(l - 1); }
+	long get(final int k){ return sum(k) - sum(k - 1); }
+	void add(int k, final long x) {
+		for(++k; k < n; k += k & -k) {
+			data[k] += x;
+		}
+	}
+	void imos(final int l, final int r, long x) {
+		add(l, x);
+		add(r + 1, -x);
+	}
+	private int lg(final int n){ return 63 - Integer.numberOfLeadingZeros(n); }
+	int lowerBound(long w) {
+		if(w <= 0) {
+			return 0;
+		}
+		int x = 0;
+		for(int k = 1 << lg(n); k > 0; k >>= 1) {
+			if(x + k <= n - 1 && data[x + k] < w) {
+				w -= data[x + k];
+				x += k;
+			}
+		}
+		return x;
+	}
+	int upper_bound(long w) {
+		if(w < 0) {
+			return 0;
+		}
+		int x = 0;
+		for(int k = 1 << lg(n); k > 0; k >>= 1) {
+			if(x + k <= n - 1 && data[x + k] <= w) {
+				w -= data[x + k];
+				x += k;
+			}
+		}
+		return x;
+	}
+}
+
+abstract class SegmentTree {
+	private int n = 1, rank = 0, fini;
+	final BinaryOperator<Long> op;
+	final long e;
+	private long[] dat;
+	SegmentTree(final int fini, final BinaryOperator<Long> op, final long e) {
+		this.fini = fini;
+		this.op = op;
+		this.e = e;
+		while(this.fini > n) {
+			n <<= 1;
+			rank++;
+		}
+		dat = new long[2 * n];
+		Arrays.fill(dat, e);
+	}
+	void update(int i, final long x) {
+		i += n;
+		dat[i] = x;
+		i >>= 1;
+		while(i > 0) {
+			dat[i] = op.apply(dat[2 * i], dat[2 * i + 1]);
+		}
+	}
+	void add(int i, final long x) {
+		i += n;
+		dat[i] += x;
+		i >>= 1;
+		while(i > 0) {
+			dat[i] = op.apply(dat[2 * i], dat[2 * i + 1]);
+		}
+	}
+	long query(int a, int b) {
+		long l=e,r=e;
+		for(a += n, b += n; a < b; a >>= 1, b >>= 1) {
+			if(a % 2 == 1) {
+				l = op.apply(l, dat[a++]);
+			}
+			if(b % 2 == 1) {
+				r = op.apply(dat[--b], r);
+			}
+		}
+		return op.apply(l,r);
+	}
+	int findLeft(int r, final Predicate<Long> fn) {
+		if(r == 0) {
+			return 0;
+		}
+		int h = 0, i = r + n;
+		long val = e;
+		for(; h <= rank; h++) {
+			if(i >> (h & 1) > 0) {
+				final long val2 = op.apply(val, dat[i >> (h ^ 1)]);
+				if(fn.test(val2)){
+					i -= 1 << h;
+					if(i == n) {
+						return 0;
+					}
+					val = val2;
+				}
+				else {
+					break;
+				}
+			}
+		}
+		for(; h-- > 0;) {
+			long val2 = op.apply(val, dat[(i >> h) - 1]);
+			if(fn.test(val2)){
+				i -= 1 << h;
+				if(i == n) {
+					return 0;
+				}
+				val = val2;
+			}
+		}
+		return i - n;
+	}
+	int findRight(int l, final Predicate<Long> fn) {
+		if(l == fini) {
+			return fini;
+		}
+		int h = 0, i = l + n;
+		long val = e;
+		for(; h <= rank; h++) {
+			if(i >> (h & 1) > 0){
+				long val2 = op.apply(val, dat[i >> h]);
+				if(fn.test(val2)){
+					i += 1 << h;
+					if(i == n * 2) {
+						return fini;
+					}
+					val = val2;
+				}
+				else {
+					break;
+				}
+			}
+		}
+		for(; h-- > 0;) {
+			long val2 = op.apply(val, dat[i>>h]);
+			if(fn.test(val2)) {
+				i += 1 << h;
+				if(i == n * 2) {
+					return fini;
+				}
+				val = val2;
+			}
+		}
+		return Math.min(i - n, fini);
+	}
+}
+
+class SparseTable {
+	private long[][] st;
+	private int[] lookup;
+	private BinaryOperator<Long> op;
+	SparseTable(final long[] a, final BinaryOperator<Long> op) {
+		this.op = op;
+		int b = 0;
+		while((1 << b) <= a.length) {
+			++b;
+		}
+		st = new long[b][1 << b];
+		for(int i = 0; i < a.length; i++) {
+			st[0][i] = a[i];
+		}
+		for(int i = 1; i < b; i++) {
+			for(int j = 0; j + (1 << i) <= (1 << b); j++) {
+				st[i][j] = op.apply(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+			}
+		}
+		lookup = new int[a.length + 1];
+		for(int i = 2; i < lookup.length; i++) {
+			lookup[i] = lookup[i >> 1] + 1;
+		}
+	}
+	long query(final int l, final int r) {
+		final int b = lookup[r - l];
+		return op.apply(st[b][l], st[b][r - (1 << b)]);
+	}
+	int minLeft(final int x, final Predicate<Long> fn) {
+		if(x == 0) {
+			return 0;
+		}
+		int ok = x, ng = -1;
+		while(Math.abs(ok - ng) > 1) {
+			final int mid = (ok + ng) / 2;
+			if(fn.test(query(mid, x) - 1)) {
+				ok = mid;
+			}
+			else {
+				ng = mid;
+			}
+		}
+		return ok;
+	}
+	int maxRight(final int x, final Predicate<Long> fn) {
+		if(x == lookup.length - 1) {
+			return lookup.length - 1;
+		}
+		int ok = x, ng = lookup.length;
+		while(Math.abs(ok - ng) > 1) {
+			int mid = (ok + ng) / 2;
+			if(fn.test(query(x, mid))) {
+				ok = mid;
+			}
+			else {
+				ng = mid;
+			}
+		}
+		return ok;
 	}
 }
