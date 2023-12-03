06ab8645a884c89375ccb26546ba5fd65ccf35e5
VvyLw <owl1465fu9@gmail.com>
2023-12-03 13:05:02 +0900

    kth-root-integer test

---
+++ test/kthrooti.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/kth_root_integer"
+#include <iostream>
+#include <C++/math.hpp>
+void solve() {
+    ul a;
+    int k;
+    std::cin >> a >> k;
+    std::cout << kthrooti(a, k) << '\n';
+}
+int main() {
+    std::cin.tie(nullptr) -> sync_with_stdio(false);
+    int t;
+    std::cin >> t;
+    while(t--) {
+        solve();
+    }
+}
