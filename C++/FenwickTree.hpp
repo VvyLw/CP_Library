6ae10e4f33b0a4e80c853d2c9803dc4c4d5528ef
VvyLw <owl1465fu9@gmail.com>
2023-12-11 13:19:03 +0900

    inv_num upgrade

--- C++/FenwickTree.hpp
+++ C++/FenwickTree.hpp
@@ -59,12 +59,20 @@ public:
     }
 };
 
-template <class T> inline int inv_num(const std::vector<T> &a) {
+#include <unordered_map>
+#include <algorithm>
+template <class T> inline long long inv_num(const std::vector<T> &a) {
+    auto b = a;
+    std::sort(b.begin(), b.end());
+    std::unordered_map<T, int> id;
+    for(size_t i = 0; i < b.size(); ++i) {
+        id[b[i]] = i;
+    }
     FenwickTree<T> bit(a.size());
-    int res = 0;
+    long long res = 0;
     for(size_t i = 0; i < a.size(); ++i) {
-        res += i - bit.sum(a[i]);
-        bit.add(a[i], 1);
+        res += i - bit.sum(id[a[i]]);
+        bit.add(id[a[i]], 1);
     }
     return res;
 }
