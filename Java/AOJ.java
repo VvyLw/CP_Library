26624fc25c504950750c87e19edee44c9a96395f
VvyLw <owl1465fu9@gmail.com>
2024-01-06 16:57:25 +0900

    for yukicoder, aoj

---
+++ Java/AOJ.java
@@ -0,0 +1,531 @@
+import java.io.Closeable;
+import java.io.Flushable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Formatter;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+final class Main {
+	public static void main(final String[] args) {
+		IntStream.range(0, VvyLw.multi ? VvyLw.sc.ni() : 1).forEach(i -> VvyLw.solve());
+		VvyLw.o.flush();
+		VvyLw.sc.close();
+		VvyLw.o.close();
+		VvyLw.dbg.close();
+	}
+}
+
+final class VvyLw extends Utility {
+	static final MyScanner sc = new MyScanner(System.in);
+	static final MyPrinter o = new MyPrinter(System.out, false);
+	static final MyPrinter dbg = new MyPrinter(System.err, true);
+	static final boolean multi = false;
+	static final int inf = 1 << 30;
+	static final long linf = (1L << 61) - 1;
+	static final double eps = 1e-18;
+	@SuppressWarnings("unused")
+	private static final int[] dx = {0, -1, 1, 0, 0, -1, -1, 1, 1};
+	@SuppressWarnings("unused")
+	private static final int[] dy = {0, 0, 0, -1, 1, -1, 1, -1, 1};
+	@SuppressWarnings("unused")
+	private static final int mod998 = 998244353;
+	@SuppressWarnings("unused")
+	private static final int mod107 = (int)1e9 + 7;
+	static final void solve() {
+		
+	}
+}
+class Utility {
+	protected static final String yes(final boolean ok){ return ok ? "Yes" : "No"; }
+	protected static final String no(final boolean ok){ return yes(!ok); }
+	protected static final long sqr(final long x){ return x * x; }
+	protected static final int mod(final int n, final int m){ return (n + m) % m; }
+	protected static final int mod(final long n, final int m){ return (int) (n + m) % m; }
+	protected static final long mod(final long n, final long m){ return (n + m) % m; }
+	protected static final long ceil(final long a, final long b){ return (a - 1) / b + 1; }
+	protected static final double round(final double a, final long b, final int c) {
+		final long d = pow(10, c);
+		return Math.rint((a * d) / b) / d;
+	}
+	protected static final long pow(long a, int b) {
+		long res = 1;
+		while(b > 0) {
+			if(b % 2 == 1) {
+				res *= a;
+			}
+			a *= a;
+			b >>= 1;
+		}
+		return res;
+	}
+	protected static final long pow(long a, long b, final long m) {
+		long res = 1;
+		while(b > 0) {
+			if(b % 2 == 1) {
+				res *= a;
+				res = mod(res, m);
+			}
+			a *= a;
+			a = mod(a, m);
+			b >>= 1;
+		}
+		return res;
+	}
+	protected static final long lcm(final long a, final long b){ return a * b / gcd(a, b); }
+	protected static final long lcm(final int... a){ return Arrays.stream(a).mapToLong(i -> i).reduce(1, (x, y) -> lcm(x, y)); }
+	protected static final long lcm(final long... a){ return Arrays.stream(a).reduce(1, (x, y) -> lcm(x, y)); }
+	protected static final long gcd(final long a, final long b){ return b > 0 ? gcd(b, a % b) : a; }
+	protected static final int gcd(final int... a){ return Arrays.stream(a).reduce(0, (x, y) -> (int) gcd(x, y)); }
+	protected static final long gcd(final long... a){ return Arrays.stream(a).reduce(0, (x, y) -> gcd(x, y)); }
+	protected static final int min(final int... a){ return Arrays.stream(a).reduce(Integer.MAX_VALUE, (x, y) -> Math.min(x, y)); }
+	protected static final long min(final long... a){ return Arrays.stream(a).reduce(Long.MAX_VALUE, (x, y) -> Math.min(x, y)); }
+	protected static final int max(final int... a){ return Arrays.stream(a).reduce(Integer.MIN_VALUE, (x, y) -> Math.max(x, y)); }
+	protected static final long max(final long... a){ return Arrays.stream(a).reduce(Long.MIN_VALUE, (x, y) -> Math.max(x, y)); }
+	protected static final boolean scope(final int l, final int x, final int r){ return l <= x && x <= r; }
+	protected static final boolean scope(final long l, final long x, final long r){ return l <= x && x <= r; }
+	protected static final boolean scope(final double l, final double x, final double r){ return l <= x && x <= r; }
+	protected static final String sorted(final String s){ return s.chars().sorted().mapToObj(Character::toString).collect(Collectors.joining()); }
+	protected static final int[] sorted(final int[] a){ return Arrays.stream(a).sorted().toArray(); }
+	protected static final long[] sorted(final long[] a){ return Arrays.stream(a).sorted().toArray(); }
+	protected static final double[] sorted(final double[] a){ return Arrays.stream(a).sorted().toArray(); }
+	protected static final void swap(final int[] a, final int i, final int j) {
+		a[i] ^= a[j];
+		a[j] ^= a[i];
+		a[i] ^= a[j];
+	}
+	protected static final void swap(final long[] a, final int i, final int j) {
+		a[i] ^= a[j];
+		a[j] ^= a[i];
+		a[i] ^= a[j];
+	}
+	protected static final void swap(final double[] a, final int i, final int j) {
+		final double tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+	}
+	protected static final void swap(final char[] a, final int i, final int j) {
+		a[i] ^= a[j];
+		a[j] ^= a[i];
+		a[i] ^= a[j];
+	}
+	protected static final void swap(final boolean[] a, final int i, final int j) {
+		a[i] ^= a[j];
+		a[j] ^= a[i];
+		a[i] ^= a[j];
+	}
+	protected static final void swap(final Object[] a, final int i, final int j) {
+		final Object tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+	}
+	protected static final void swap(final int[] a, final int[] b) {
+		assert a.length == b.length;
+		final int n = a.length;
+		final int[] c = a.clone();
+		System.arraycopy(b, 0, a, 0, n);
+		System.arraycopy(c, 0, b, 0, n);
+	}
+	protected static final void swap(final long[] a, final long[] b) {
+		assert a.length == b.length;
+		final int n = a.length;
+		final long[] c = a.clone();
+		System.arraycopy(b, 0, a, 0, n);
+		System.arraycopy(c, 0, b, 0, n);
+	}
+	protected static final void swap(final double[] a, final double[] b) {
+		assert a.length == b.length;
+		final int n = a.length;
+		final double[] c = a.clone();
+		System.arraycopy(b, 0, a, 0, n);
+		System.arraycopy(c, 0, b, 0, n);
+	}
+	protected static final void swap(final char[] a, final char[] b) {
+		assert a.length == b.length;
+		final int n = a.length;
+		final char[] c = a.clone();
+		System.arraycopy(b, 0, a, 0, n);
+		System.arraycopy(c, 0, b, 0, n);
+	}
+	protected static final void swap(final boolean[] a, final boolean[] b) {
+		assert a.length == b.length;
+		final int n = a.length;
+		final boolean[] c = a.clone();
+		System.arraycopy(b, 0, a, 0, n);
+		System.arraycopy(c, 0, b, 0, n);
+	}
+	protected static final void swap(final Object[] a, final Object[] b) {
+		assert a.length == b.length;
+		final int n = a.length;
+		final Object[] c = a.clone();
+		System.arraycopy(b, 0, a, 0, n);
+		System.arraycopy(c, 0, b, 0, n);
+	}
+	protected static final <F extends Comparable<? super F>, S extends Comparable<? super S>> List<F> first(final List<Pair<F, S>> p){ return p.stream().map(i -> i.first).collect(Collectors.toList()); }
+	protected static final <F extends Comparable<? super F>, S extends Comparable<? super S>> List<S> second(final List<Pair<F, S>> p){ return p.stream().map(i -> i.second).collect(Collectors.toList()); }
+	protected interface TriFunction<T, U, V, W> {
+		public W apply(final T a, final U b, final V c);
+	}
+	protected interface QuadFunction<A, B, C, D, E> {
+		public E apply(final A a, final B b, final C c, final D d);
+	}
+	protected interface RecursiveFunction<T, U> {
+		public U apply(final RecursiveFunction<T, U> rec, final T n);
+	}
+	protected interface RecursiveBiFunction<T, U, V> {
+		public V apply(final RecursiveBiFunction<T, U, V> rec, final T n, final U m);
+	}
+	protected interface RecursiveTriFunction<T, U, V, W> {
+		public W apply(final RecursiveTriFunction<T, U, V, W> rec, final T p, final U q, final V r);
+	}
+	protected interface RecursiveUnaryOperator<T> {
+		public T apply(final RecursiveUnaryOperator<T> rec, final T n);
+	}
+	protected interface RecursiveBinaryOperator<T> {
+		public T apply(final RecursiveBinaryOperator<T> rec, final T a, final T b);
+	}
+}
+
+final class MyScanner implements Closeable, AutoCloseable {
+	private int pos, lim;
+	private final byte[] buf;
+	private final InputStream is;
+	MyScanner(final InputStream is) {
+		this.is = is;
+		pos = lim = 0;
+		buf = new byte[1 << 17];
+	}
+	private final boolean isPunct(final byte bt){ return !Utility.scope(33, bt, 126); }
+	private final boolean isNum(final byte bt){ return Utility.scope('0', bt, '9'); }
+	private final byte read() {
+		if(pos == lim && lim != -1) {
+			try {
+				lim = is.read(buf);
+				pos = 0;
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return buf[pos++];
+	}
+	private final byte next() {
+		byte bt;
+		while(isPunct(bt = read())){}
+		return bt;
+	}
+	final int ni(){ return Math.toIntExact(nl()); }
+	final long nl() {
+		byte c = next();
+		final boolean neg = c == '-';
+		if(neg) {
+			c = read();
+		}
+		assert(isNum(c));
+		long res = c - '0';
+		while(isNum(c = read())) {
+			res = 10 * res + c - '0';
+		}
+		return neg ? -res : res;
+	}
+	final double nd(){ return Double.parseDouble(ns()); }
+	final char nc(){ return (char) next(); }
+	final String ns() {
+		final StringBuilder sb = new StringBuilder();
+		byte c = next();
+		while(!isPunct(c)) {
+			sb.append((char) c);
+			c = read();
+		}
+		return sb.toString();
+	}
+	final BigInteger nb(){ return new BigInteger(ns()); }
+	final int[] ni(final int n) {
+		final int[] a = new int[n];
+		IntStream.range(0, n).forEach(i -> a[i] = ni());
+		return a;
+	}
+	final long[] nl(final int n) {
+		final long[] a = new long[n];
+		IntStream.range(0, n).forEach(i -> a[i] = nl());
+		return a;
+	}
+	final double[] nd(final int n) {
+		final double[] a = new double[n];
+		IntStream.range(0, n).forEach(i -> a[i] = nd());
+		return a;
+	}
+	final char[] nc(final int n) {
+		final char[] a = new char[n];
+		IntStream.range(0, n).forEach(i -> a[i] = nc());
+		return a;
+	}
+	final String[] ns(final int n) {
+		final String[] a = new String[n];
+		IntStream.range(0, n).forEach(i -> a[i] = ns());
+		return a;
+	}
+	final BigInteger[] nb(final int n) {
+		final BigInteger[] a = new BigInteger[n];
+		IntStream.range(0, n).forEach(i -> a[i] = nb());
+		return a;
+	}
+	final int[][] ni(final int h, final int w) {
+		final int[][] a = new int[h][w];
+		IntStream.range(0, h).forEach(i -> a[i] = ni(w));
+		return a;
+	}
+	final long[][] nl(final int h, final int w) {
+		final long[][] a = new long[h][w];
+		IntStream.range(0, h).forEach(i -> a[i] = nl(w));
+		return a;
+	}
+	final double[][] nd(final int h, final int w) {
+		final double[][] a = new double[h][w];
+		IntStream.range(0, h).forEach(i -> a[i] = nd(w));
+		return a;
+	}
+	final char[][] nc(final int h, final int w) {
+		final char[][] a = new char[h][w];
+		IntStream.range(0, h).forEach(i -> a[i] = nc(w));
+		return a;
+	}
+	final String[][] ns(final int h, final int w) {
+		final String[][] a = new String[h][w];
+		IntStream.range(0, h).forEach(i -> a[i] = ns(w));
+		return a;
+	}
+	final BigInteger[][] nb(final int h, final int w) {
+		final BigInteger[][] a = new BigInteger[h][w];
+		IntStream.range(0, h).forEach(i -> a[i] = nb(w));
+		return a;
+	}
+	final String line() {
+		final StringBuilder sb = new StringBuilder();
+		byte c;
+		while((c = read()) != '\n') {
+			sb.append(c);
+		}
+		return sb.toString();
+	}
+	@Override
+	public final void close() {
+		try {
+			is.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+final class MyPrinter implements Closeable, Flushable, AutoCloseable {
+	private OutputStream os;
+	private final boolean autoFlush;
+	private final byte[] buf;
+	private int pos;
+	MyPrinter(final OutputStream os, final boolean autoFlush){
+		this.os = os;
+		this.autoFlush = autoFlush;
+		buf = new byte[1 << 17];
+		pos = 0;
+	}
+	private final void write(final byte bt) {
+		buf[pos++] = bt;
+		if(pos == buf.length) {
+			flush();
+		}
+	}
+	private final void newLine() {
+		write((byte) '\n');
+		if(autoFlush) {
+			flush();
+		}
+	}
+	final void print(final Object arg) {
+		if(arg instanceof String) {
+			final String s = (String) arg;
+			for(final char c: s.toCharArray()) {
+				write((byte) c);
+			}
+		} else {
+			print(String.valueOf(arg));
+		}
+	}
+	final void printf(final String fmt, final Object... args){ print(new Formatter().format(fmt, args)); }
+	private final void println(final Object arg) {
+		print(arg);
+		newLine();
+	}
+	final void out(){ newLine(); }
+	final void out(final Object head, final Object... tail) {
+		print(head);
+		for(final var el: tail) {
+			print(" " + el);
+		}
+		newLine();
+	}
+	final <F extends Comparable<? super F>, S extends Comparable<? super S>> void out(final Pair<F, S> arg){ println(arg.first + " " + arg.second); }
+	final void out(final int[] args){ IntStream.range(0, args.length).forEach(i -> print(args[i] + (i + 1 < args.length ? " " : "\n"))); }
+	final void out(final long[] args){ IntStream.range(0, args.length).forEach(i -> print(args[i] + (i + 1 < args.length ? " " : "\n"))); }
+	final void out(final double[] args){ IntStream.range(0, args.length).forEach(i -> print(args[i] + (i + 1 < args.length ? " " : "\n"))); }
+	final void out(final boolean[] args){ IntStream.range(0, args.length).forEach(i -> print(args[i] + (i + 1 < args.length ? " " : "\n"))); }
+	final void out(final char[] args){ IntStream.range(0, args.length).forEach(i -> print(args[i] + (i + 1 < args.length ? " " : "\n"))); }
+	final void out(final Object[] args){ IntStream.range(0, args.length).forEach(i -> print(args[i] + (i + 1 < args.length ? " " : "\n"))); }
+	final <E> void out(final Collection<E> args) {
+		int i = 0;
+		for(final var el: args) {
+			print(el + (++i == args.size() ? "\n" : " "));
+		}
+	}
+	final void outl(final Object head, final Object... tail) {
+		out(head);
+		Arrays.stream(tail).forEach(this::println);
+	}
+	final void outl(final int[] args){ Arrays.stream(args).forEach(this::println); }
+	final void outl(final int[][] args){ IntStream.range(0, args.length).forEach(i -> out(args[i])); }
+	final void outl(final long[] args){ Arrays.stream(args).forEach(this::println); }
+	final void outl(final long[][] args){ IntStream.range(0, args.length).forEach(i -> out(args[i])); }
+	final void outl(final double[] args){ Arrays.stream(args).forEach(this::println); }
+	final void outl(final double[][] args){ IntStream.range(0, args.length).forEach(i -> out(args[i])); }
+	final void outl(final boolean[] args){ IntStream.range(0, args.length).forEach(i -> out(args[i])); }
+	final void outl(final boolean[][] args){ IntStream.range(0, args.length).forEach(i -> out(args[i])); }
+	final void outl(final char[] args){ IntStream.range(0, args.length).forEach(i -> out(args[i])); }
+	final void outl(final Object[] args){ Arrays.stream(args).forEach(this::println); }
+	final <E> void outl(final Collection<E> args){ args.stream().forEach(this::println); }
+	final void fin(final Object head, final Object... tail) {
+		out(head, tail);
+		flush();
+		System.exit(0);
+	}
+	final void fin(final int[] args) {
+		out(args);
+		flush();
+		System.exit(0);
+	}
+	final void fin(final long[] args) {
+		out(args);
+		flush();
+		System.exit(0);
+	}
+	final void fin(final double[] args) {
+		out(args);
+		flush();
+		System.exit(0);
+	}
+	final void fin(final boolean[] args) {
+		out(args);
+		flush();
+		System.exit(0);
+	}
+	final void fin(final char[] args) {
+		out(args);
+		flush();
+		System.exit(0);
+	}
+	final void fin(final Object[] args) {
+		out(args);
+		flush();
+		System.exit(0);
+	}
+	final <T> void fin(final List<T> args) {
+		out(args);
+		flush();
+		System.exit(0);
+	}
+	final void ende(final int[] args) {
+		outl(args);
+		flush();
+		System.exit(0);
+	}
+	final void ende(final long[] args) {
+		outl(args);
+		flush();
+		System.exit(0);
+	}
+	final void ende(final double[] args) {
+		outl(args);
+		flush();
+		System.exit(0);
+	}
+	final void ende(final boolean[] args) {
+		outl(args);
+		flush();
+		System.exit(0);
+	}
+	final void ende(final char[] args) {
+		outl(args);
+		flush();
+		System.exit(0);
+	}
+	final void ende(final Object[] args) {
+		outl(args);
+		flush();
+		System.exit(0);
+	}
+	final <E> void ende(final Collection<E> args) {
+		outl(args);
+		flush();
+		System.exit(0);
+	}
+	@Override
+	public final void flush() {
+		try {
+			os.write(buf, 0, pos);
+			pos = 0;
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	@Override
+	public final void close() {
+		if(os == null) {
+			return;
+		}
+		try {
+			os.close();
+			os = null;
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class Pair<F extends Comparable<? super F>, S extends Comparable<? super S>> implements Comparable<Pair<F, S>> {
+	public F first;
+	public S second;
+	Pair(final F first, final S second) {
+		this.first = first;
+		this.second = second;
+	}
+	@Override
+	public final boolean equals(final Object o) {
+		if(this == o) {
+			return true;
+		}
+		if(o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		final Pair<?, ?> p = (Pair<?, ?>) o;
+		if(!first.equals(p.first)) {
+			return false;
+		}
+		return second.equals(p.second);
+	}
+	@Override
+	public final int hashCode(){ return 31 * first.hashCode() + second.hashCode(); }
+	@Override
+	public final String toString(){ return "(" + first + ", " + second + ")"; }
+	public static final <F extends Comparable<? super F>, S extends Comparable<? super S>> Pair<F, S> of(final F a, final S b){ return new Pair<>(a, b); }
+	final Pair<S, F> swap(){ return Pair.of(second, first); }
+	@Override
+	public final int compareTo(final Pair<F, S> p) {
+		if(first.compareTo(p.first) == 0) {
+			return second.compareTo(p.second);
+		}
+		return first.compareTo(p.first);
+	}
+}
