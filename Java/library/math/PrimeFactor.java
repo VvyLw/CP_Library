cd75a012454f954078089d7ac7ac25d9922db380
VvyLw <owl1465fu9@gmail.com>
2024-01-25 10:40:17 +0900

    modified

--- Java/library/math/PrimeFactor.java
+++ Java/library/math/PrimeFactor.java
@@ -12,11 +12,12 @@ public final class PrimeFactor {
 	public PrimeFactor(final int n) {
 		spf = Utility.iota(n + 1);
 		for(int i = 2; i * i <= n; ++i) {
-			if(spf[i] == i) {
-				for(int j = i * i; j <= n; j += i) {
-					if(spf[j] == j) {
-						spf[j] = i;
-					}
+			if(spf[i] != i) {
+				continue;
+			}
+			for(int j = i * i; j <= n; j += i) {
+				if(spf[j] == j) {
+					spf[j] = i;
 				}
 			}
 		}
