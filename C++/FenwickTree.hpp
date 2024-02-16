bee330add71a90ca0c96f6b017c8973073c72921
VvyLw <owl1465fu9@gmail.com>
2024-02-16 09:59:43 +0900

    modified

--- C++/FenwickTree.hpp
+++ C++/FenwickTree.hpp
@@ -3,59 +3,62 @@
 #include <vector>
 template <class T> struct FenwickTree {
 private:
-    int N;
+    int n;
     std::vector<T> data;
     void init(const size_t size) {
-        N = size + 2;
-        data.assign(N + 1, {});
+        n = size + 2;
+        data.resize(n + 1);
     }
 public:
     FenwickTree(){}
-    FenwickTree(size_t size){ init(size); }
+    FenwickTree(const size_t size){ init(size); }
     FenwickTree(const std::vector<T> &a) {
         init(a.size());
         for(size_t i = 0; i < a.size(); ++i) {
             add(i, a[i]);
         }
     }
-    // get sum of [0,k]
     T sum(int k) const {
-        if(k < 0) return T{};  // return 0 if k < 0
-        T ret{};
-        for(++k; k > 0; k -= k & -k) ret += data[k];
+        if(k < 0) {
+            return 0;
+        }
+        T ret = 0;
+        for(++k; k > 0; k -= k & -k) {
+            ret += data[k];
+        }
         return ret;
     }
-    // getsum of [l,r]
     inline T sum(int l, int r) const { return sum(r) - sum(l - 1); }
-    // get value of k
     inline T operator[](int k) const { return sum(k) - sum(k - 1); }
-    // data[k] += x
-    void add(int k, T x) {
-        for(++k; k < N; k += k & -k) data[k] += x;
+    void add(int k, const T &x) {
+        for(++k; k < n; k += k & -k) {
+            data[k] += x;
+        }
     }
-    // range add x to [l,r]
-    void add(int l, int r, T x) {
+    void add(const int l, const int r, const T& x) {
         add(l, x);
         add(r + 1, -x);
     }
-    // minimize i s.t. sum(i) >= w
     int lower_bound(T w) {
-        if(w <= 0) return 0;
+        if(w <= 0) {
+            return 0;
+        }
         int x = 0;
-        for(int k = 1 << std::__lg(N); k; k >>= 1) {
-            if(x + k <= N - 1 && data[x + k] < w) {
+        for(int k = 1 << std::__lg(n); k; k >>= 1) {
+            if(x + k <= n - 1 && data[x + k] < w) {
                 w -= data[x + k];
                 x += k;
             }
         }
         return x;
     }
-    // minimize i s.t. sum(i) > w
     int upper_bound(T w) {
-        if(w < 0) return 0;
+        if(w < 0) {
+            return 0;
+        }
         int x = 0;
-        for(int k = 1 << std::__lg(N); k; k >>= 1) {
-            if(x + k <= N - 1 && data[x + k] <= w) {
+        for(int k = 1 << std::__lg(n); k; k >>= 1) {
+            if(x + k <= n - 1 && data[x + k] <= w) {
                 w -= data[x + k];
                 x += k;
             }
@@ -72,7 +75,7 @@ template <class T> inline long long inv_num(const std::vector<T> &a) {
     }
     std::sort(p.begin(), p.end());
     std::vector<int> id(a.size());
-    for(int i = 0; i < a.size(); ++i) {
+    for(size_t i = 0; i < a.size(); ++i) {
         id[p[i].second] = i;
     }
     FenwickTree<T> bit(a.size());
@@ -85,5 +88,5 @@ template <class T> inline long long inv_num(const std::vector<T> &a) {
 }
 /**
  * @brief Binary Indexed Tree
- * @see https://github.com/NyaanNyaan/library/blob/master/data-structure/binary-indexed-tree.hpp
+ * @see https://nyaannyaan.github.io/library/data-structure/binary-indexed-tree.hpp
  */
