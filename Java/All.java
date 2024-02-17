1c690f567dc1790c65553fb055b443d7aa3db680
VvyLw <owl1465fu9@gmail.com>
2024-02-17 10:49:04 +0900

    prefixsum2d

--- Java/All.java
+++ Java/All.java
@@ -2912,6 +2912,56 @@ final class PrefixSum extends InclusiveScan {
 		s = Utility.rotate(Arrays.copyOf(s, n + 1), 1);
 	}
 }
+final class PrefixSum2D {
+	private final int h, w;
+	private final long[][] data;
+	PrefixSum2D(final int h, final int w) {
+		this.h = h + 3;
+		this.w = w + 3;
+		data = new long[this.h][this.w];
+	}
+	PrefixSum2D(final int[][] a) {
+		this(a.length, a[0].length);
+		for(int i = 0; i < h; ++i) {
+			for(int j = 0; j < w; ++j) {
+				add(i, j, a[i][j]);
+			}
+		}
+		build();
+	}
+	PrefixSum2D(final long[][] a) {
+		this(a.length, a[0].length);
+		for(int i = 0; i < h; ++i) {
+			for(int j = 0; j < w; ++j) {
+				add(i, j, a[i][j]);
+			}
+		}
+		build();
+	}
+	final void add(int i, int j, final long x) {
+		i++;
+		j++;
+		if(i >= h || j >= w) {
+			return;
+		}
+		data[i][j] += x;
+	}
+	final void add(final int i1, final int j1, final int i2, final int j2, final long x) {
+		add(i1, j1, x);
+		add(i1, j2, -x);
+		add(i2, j1, -x);
+		add(i2, j2, x);
+	}
+	final void build() {
+		for(int i = 1; i < h; ++i) {
+			for(int j = 1; j < w; ++j) {
+				data[i][j] += data[i][j - 1] + data[i - 1][j] - data[i - 1][j - 1];
+			}
+		}
+	}
+	final long get(final int i1, final int j1, final int i2, final int j2){ return data[i2][j2] - data[i1][j2] - data[i2][j1] + data[i1][j1]; }
+	final long get(final int i, final int j){ return data[i + 1][j + 1]; }
+}
 
 final class SuffixArray extends ArrayList<Integer> {
 	private final String vs;
---
+++ Java/library/other/PrefixSum2D.java
@@ -0,0 +1,96 @@
+package library.other;
+
+/**
+ * 二次元累積和
+ * @see <a href="https://nyaannyaan.github.io/library/verify/verify-aoj-dsl/aoj-dsl-5-b.test.cpp">参考元</a>
+ */
+public final class PrefixSum2D {
+	private final int h, w;
+	private final long[][] data;
+	/**
+	 * コンストラクタ
+	 * @param h
+	 * @param w
+	 */
+	public PrefixSum2D(final int h, final int w) {
+		this.h = h + 3;
+		this.w = w + 3;
+		data = new long[this.h][this.w];
+	}
+	/**
+	 * コンストラクタ
+	 * @param a
+	 */
+	public PrefixSum2D(final int[][] a) {
+		this(a.length, a[0].length);
+		for(int i = 0; i < h; ++i) {
+			for(int j = 0; j < w; ++j) {
+				add(i, j, a[i][j]);
+			}
+		}
+	}
+	/**
+	 * コンストラクタ
+	 * @param a
+	 */
+	public PrefixSum2D(final long[][] a) {
+		this(a.length, a[0].length);
+		for(int i = 0; i < h; ++i) {
+			for(int j = 0; j < w; ++j) {
+				add(i, j, a[i][j]);
+			}
+		}
+	}
+	/**
+	 * (i, j)にxを加算
+	 * @param i
+	 * @param j
+	 * @param x
+	 */
+	public final void add(int i, int j, final long x) {
+		i++;
+		j++;
+		if(i >= h || j >= w) {
+			return;
+		}
+		data[i][j] += x;
+	}
+	/**
+	 * [i1, i2), [j1, j2)にxを加算
+	 * @param i1
+	 * @param j1
+	 * @param i2
+	 * @param j2
+	 * @param x
+	 */
+	public final void add(final int i1, final int j1, final int i2, final int j2, final long x) {
+		add(i1, j1, x);
+		add(i1, j2, -x);
+		add(i2, j1, -x);
+		add(i2, j2, x);
+	}
+	/**
+	 * 構築
+	 */
+	public final void build() {
+		for(int i = 1; i < h; ++i) {
+			for(int j = 1; j < w; ++j) {
+				data[i][j] += data[i][j - 1] + data[i - 1][j] - data[i - 1][j - 1];
+			}
+		}
+	}
+	/**
+	 * @param i1
+	 * @param j1
+	 * @param i2
+	 * @param j2
+	 * @return [i1, i2), [j1, j2)の矩形和
+	 */
+	public final long get(final int i1, final int j1, final int i2, final int j2){ return data[i2][j2] - data[i1][j2] - data[i2][j1] + data[i1][j1]; }
+	/**
+	 * @param i
+	 * @param j
+	 * @return (i, j)の要素
+	 */
+	public final long get(final int i, final int j){ return data[i + 1][j + 1]; }
+}
