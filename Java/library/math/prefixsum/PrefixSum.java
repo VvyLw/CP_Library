d07382b42f2377f15d9954071bc620e17a758c2b
VvyLw <owl1465fu9@gmail.com>
2024-02-28 10:21:05 +0900

    inclusivescan

--- Java/library/math/prefixsum/PrefixSum.java
+++ Java/library/math/prefixsum/PrefixSum.java
@@ -7,6 +7,7 @@ import library.other.InclusiveScan;
 
 /**
  * 累積和クラス
+ * @see InclusiveScan
  */
 public final class PrefixSum extends InclusiveScan {
 	/**
@@ -25,4 +26,11 @@ public final class PrefixSum extends InclusiveScan {
 		super(a, (x, y) -> x + y);
 		s = Utility.rotate(Arrays.copyOf(s, n + 1), 1);
 	}
+	/**
+	 * 主に累積和の差を求めるのに使う
+	 * @param l
+	 * @param r
+	 * @return 半開区間[l, r)におけるs[l]とs[r]との差
+	 */
+	public final long query(final int l, final int r){ return s[r] - s[l]; }
 }
--- Java/library/other/InclusiveScan.java
+++ Java/library/other/InclusiveScan.java
@@ -3,10 +3,9 @@ package library.other;
 import java.util.Arrays;
 import java.util.function.LongBinaryOperator;
 
-import library.math.prefixsum.PrefixSum;
-
 /**
  * C++のstd::inclusive_scanに相当するクラス
+ * @see <a href="https://cpprefjp.github.io/reference/numeric/inclusive_scan.html">std::inclusive_scan</a>
  */
 public class InclusiveScan {
 	protected final int n;
@@ -32,15 +31,7 @@ public class InclusiveScan {
 		Arrays.parallelPrefix(s, op);
 	}
 	/**
-	 * @see <a href="https://cpprefjp.github.io/reference/numeric/inclusive_scan.html">std::inclusive_scan</a> 
+	 * @return InclusiveScanした後の配列
 	 */
-	public long[] get(){ return s; }
-	/**
-	 * 主に累積和の差を求めるのに使う
-	 * @param l
-	 * @param r
-	 * @return 半開区間[l, r)におけるs[l]とs[r]との差
-	 * @see PrefixSum
-	 */
-	public long query(final int l, final int r){ return s[r] - s[l]; }
+	public final long[] get(){ return s; }
 }
