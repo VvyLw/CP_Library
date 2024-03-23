571d19e1e26c323689fff084b8f7ca750e0160ff
VvyLw <owl1465fu9@gmail.com>
2024-03-23 17:15:45 +0900

    modified

--- C++/math/psum/psum.hpp
+++ C++/math/psum/psum.hpp
@@ -8,22 +8,22 @@ struct psum {
 private:
     int n;
     bool not_built;
-    vectors::vi s, imos;
+    vectors::vi s;
 public:
-    psum(const vectors::vi& v): s{0} { std::partial_sum(v.begin(), v.end(), std::back_inserter(s)); }
-    psum(const int n): n(n), not_built(true), imos(n + 1){}
+    psum(const vectors::vi& v): s{0} { std::partial_sum(v.cbegin(), v.cend(), std::back_inserter(s)); }
+    psum(const int n): n(n), not_built(true), s(n + 1){}
     vectors::vi get() const { return s; }
     // [l, r]
     ll sum(const int l, const int r) const { return s[r] - s[l]; }
-    void add(const int l, const int r, const ll x = 1) {
+    void add(const int l, const int r, const ll x) {
 		assert(not_built);
-		imos[l] += x;
-		imos[r] -= x;
+		s[l] += x;
+		s[r] -= x;
 	}
     vectors::vi build() {
 		assert(not_built);
         vectors::vi res;
-		std::partial_sum(imos.begin(), imos.end(), std::back_inserter(res));
+		std::partial_sum(s.cbegin(), s.cend(), std::back_inserter(res));
 		not_built = false;
         res.resize(n);
 		return res;
