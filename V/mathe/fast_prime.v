0f1417c6c1d86216eed05641a8b24333f355d44a
VvyLw <owl1465fu9@gmail.com>
2024-05-22 05:09:06 +0900

    fast_prime

---
+++ V/mathe/fast_prime.v
@@ -0,0 +1,102 @@
+module mathe
+
+import math as ma
+import math.big{integer_from_u32,integer_from_u64}
+import math.bits{trailing_zeros_64}
+
+fn bsf(n u64) u32 { return u32(trailing_zeros_64(n)) }
+fn b_gcd(x u64, y u64) u64 {
+	mut a,mut b:=ma.abs(x),ma.abs(y)
+	if a==0 {
+		return b
+	}
+	if b==0 {
+		return a
+	}
+	shift:=bsf(a|b)
+	a>>=bsf(a)
+	for {
+		b>>=bsf(b)
+		if a>b {
+			a,b=b,a
+		}
+		b-=a
+		if b<=0 {
+			break
+		}
+	}
+	return a<<shift
+}
+fn f(x u64, st u32, n u64) u64 { return ((integer_from_u64(x)*integer_from_u64(x)+integer_from_u32(st))%integer_from_u64(n)).str().u64() }
+fn g(n u64) !u64 {
+	if miller_rabin(n)! {
+		return n
+	}
+	if n%2==0 {
+		return 2
+	}
+	mut st:=u32(0)
+	for {
+		st++
+		mut x:=u64(st)
+		mut y:=f(u64(x),st,n)
+		for {
+			p:=b_gcd(y-x+n,n)
+			if p==0 || p==n {
+				break
+			}
+			if p!=1 {
+				return p
+			}
+			x,y=f(x,st,n),f(f(y,st,n),st,n)
+		}
+	}
+	return 0
+}
+pub fn miller_rabin(n u64) !bool {
+	if n<=1 {
+		return false
+	}
+	if n==2 {
+		return true
+	}
+	if n%2==0 {
+		return false
+	}
+	mut d:=n-1
+	for d%2==0 {
+		d/=2
+	}
+	sample:=[u32(2),3,5,7,11,13,17,19,23,29,31,37]
+	for a in sample {
+		if n<=a {
+			break
+		}
+		mut t:=d
+		mut y:=integer_from_u32(a).big_mod_pow(integer_from_u64(t),integer_from_u64(n))!.str().u64()
+		for t!=n-1 && y!=1 && y!=n-1 {
+			y=integer_from_u64(y).mod_pow(2,integer_from_u64(n)).str().u64()
+			t<<=1
+		}
+		if y!=n-1 && t%2==0 {
+            return false
+        }
+	}
+	return true
+}
+pub fn pollard_rho(n u64) ![]u64 {
+	if n==1 {
+		return []
+	}
+	x:=g(n)!
+	if x==n {
+		return [x]
+	}
+	mut l:=pollard_rho(x)!
+	r:=pollard_rho(n/x)!
+	l<<r
+	l.sort()
+	return l
+}
+
+// AtCoderのVのVersionが上がるまで
---
+++ V/mathe/fast_prime_beta.v
@@ -0,0 +1,112 @@
+module mathe
+
+import math as ma
+import math.bits{trailing_zeros_64}
+import math.unsigned{uint128_from_64}
+
+fn bsf__(n u64) u32 { return u32(trailing_zeros_64(n)) }
+fn b_gcd__(x u64, y u64) u64 {
+	mut a,mut b:=ma.abs(x),ma.abs(y)
+	if a==0 {
+		return b
+	}
+	if b==0 {
+		return a
+	}
+	shift:=bsf(a|b)
+	a>>=bsf(a)
+	for {
+		b>>=bsf(b)
+		if a>b {
+			a,b=b,a
+		}
+		b-=a
+		if b<=0 {
+			break
+		}
+	}
+	return a<<shift
+}
+fn f__(x u64, st u64, n u64) u64 { return ((uint128_from_64(x)*uint128_from_64(x)+uint128_from_64(st))%uint128_from_64(n)).str().u64() }
+fn g__(n u64) u64 {
+	if miller_rabin__(n) {
+		return n
+	}
+	if n%2==0 {
+		return 2
+	}
+	mut st:=u64(0)
+	for {
+		st++
+		mut x:=st
+		mut y:=f__(x,st,n)
+		for {
+			p:=b_gcd__(y-x+n,n)
+			if p==0 || p==n {
+				break
+			}
+			if p!=1 {
+				return p
+			}
+			x,y=f__(x,st,n),f__(f__(y,st,n),st,n)
+		}
+	}
+	return 0
+}
+fn mod_pow(a u64, b u64, mod u64) u64 {
+	mut r:=u64(1)%mod
+    mut x,mut y:=uint128_from_64(a)%uint128_from_64(mod),b
+    for y>0 {
+        if y%2==1 {
+			r=(x*uint128_from_64(r)).mod_64(mod)
+		}
+        x=(x*x)%uint128_from_64(mod)
+        y>>=1
+    }
+    return r
+}
+pub fn miller_rabin__(n u64) bool {
+	if n<=1 {
+		return false
+	}
+	if n==2 {
+		return true
+	}
+	if n%2==0 {
+		return false
+	}
+	mut d:=n-1
+	for d%2==0 {
+		d/=2
+	}
+	sample:=[u32(2),3,5,7,11,13,17,19,23,29,31,37]
+	for a in sample {
+		if n<=a {
+			break
+		}
+		mut t:=d
+		mut y:=mod_pow(a,t,n)
+		for t!=n-1 && y!=1 && y!=n-1 {
+			y=mod_pow(y,2,n)
+			t<<=1
+		}
+		if y!=n-1 && t%2==0 {
+            return false
+        }
+	}
+	return true
+}
+pub fn pollard_rho__(n u64) []u64 {
+	if n==1 {
+		return []
+	}
+	x:=g__(n)
+	if x==n {
+		return [x]
+	}
+	mut l:=pollard_rho__(x)
+	r:=pollard_rho__(n/x)
+	l<<r
+	l.sort()
+	return l
+}
