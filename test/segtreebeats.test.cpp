ed60cf3918c5a3cb8cc19e19f356b17e92581901
VvyLw <owl1465fu9@gmail.com>
2024-06-26 14:52:43 +0900

    segtreebeats

---
+++ test/segtreebeats.test.cpp
@@ -0,0 +1,32 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/range_chmin_chmax_add_range_sum"
+#include <iostream>
+#include "C++/ds/SegmentTreeBeats.hpp"
+
+int main() {
+    int n, q;
+    std::cin >> n >> q;
+    std::vector<int64_t> a(n);
+    for(auto &e: a) {
+        std::cin >> e;
+    }
+    SegmentTreeBeats seg(a);
+    while(q--) {
+        int t, l, r;
+        std::cin >> t >> l >> r;
+        if(t == 0) {
+            int64_t b;
+            std::cin >> b;
+            seg.chmin(l, r, b);
+        } else if(t == 1) {
+            int64_t b;
+            std::cin >> b;
+            seg.chmax(l, r, b);
+        } else if(t == 2) {
+            int64_t b;
+            std::cin >> b;
+            seg.add(l, r, b);
+        } else {
+            std::cout << seg.sum(l, r) << '\n';
+        }
+    }
+}
