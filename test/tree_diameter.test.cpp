e2d21b321fd2c018fb2f0192715a4d27fef1f344
VvyLw <owl1465fu9@gmail.com>
2024-07-07 10:30:18 +0900

    tree_diameter

---
+++ test/tree_diameter.test.cpp
@@ -0,0 +1,16 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/tree_diameter"
+#include <iostream>
+#include "C++/graph/diameter.hpp"
+int main() {
+    int n;
+    std::cin >> n;
+    diameter g(n, 0);
+    g.input(n - 1);
+    std::cout << g.build() << ' ';
+    const auto p = g.get_path();
+    std::cout << p.size() + 1 << '\n';
+	std::cout << p[0].src << ' ';
+	for(size_t i = 0; i < p.size(); ++i) {
+		std::cout << p[i] << " \n"[i + 1 == p.size()];
+	}
+}
