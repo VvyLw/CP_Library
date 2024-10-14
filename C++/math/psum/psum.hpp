33645cccd05526ba6e2a0df2200e0f86c355f8ce
VvyLw <owl1465fu9@gmail.com>
2024-10-14 23:23:03 +0900

    static_assert, init

--- C++/math/psum/psum.hpp
+++ C++/math/psum/psum.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <cassert>
 #include <vector>
 #include <numeric>
 namespace Heileden {
@@ -15,13 +14,13 @@ public:
     constexpr inline std::vector<int64_t> get() const noexcept { return s; }
     // [l, r]
     constexpr inline int64_t sum(const int l, const int r) const noexcept  { return s[r] - s[l]; }
-    constexpr inline void add(const int l, const int r, const int64_t x) noexcept {
-        assert(not_built);
+    constexpr inline void add(const int l, const int r, const int64_t x = 1) noexcept {
+        static_assert(not_built);
         s[l] += x;
         s[r] -= x;
     }
     constexpr inline std::vector<int64_t> build() noexcept {
-        assert(not_built);
+        static_assert(not_built);
         std::vector<int64_t> res;
         std::partial_sum(s.cbegin(), s.cend(), std::back_inserter(res));
         not_built = false;
